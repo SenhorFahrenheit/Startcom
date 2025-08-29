62ba62833ce8f55f531199ba4f7c4124cfe931ef
Pedro <pedroh.de.jesus12@gmail.com>
2025-08-29 18:20:41 -0300

    Add global session middleware

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 // Importing dependencies
 const express = require("express");
+const session = require("express-session");
 const config = require("dotenv").config();
-const connectDB = require("./config/db")
+const {connectDB, mongoose} = require("./config/db");
+const mongoStore = require("connect-mongo");
 
 
 // Creating the application
@@ -10,11 +12,32 @@ const app = express();
 // Middleware to allow JSON in request bodies
 app.use(express.json());
 
+// Middleware for session management
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'fr@se-segur4-123',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
+      maxAge: (1000 * 60 * 60 * 24 * 30),
+    },
+    store: mongoStore.create({
+      client: mongoose.connection.getClient(),
+      collectionName: 'sessao',
+      ttl: (60 * 60 * 24 * 30),
+      autoRemove: 'native',
+    }),
+  })
+);
+
 // Server port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 // Connecting to database
-connectDB()
+connectDB();
 
 const userRoutes = require("./routes/user/user_routes");
 app.use("/api/users", userRoutes);
