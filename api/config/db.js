035ec827d9a4453ed118fa11510894296af3e460
Pedro <pedroh.de.jesus12@gmail.com>
2025-08-29 18:45:09 -0300

    General changes

--- api/config/db.js
+++ api/config/db.js
@@ -4,15 +4,18 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     // Attempt to connect using the URI from .env
-await mongoose.connect(process.env.MONGO_URI); // garante que process.env.MONGO_URI está correto
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }); // garante que process.env.MONGO_URI está correto
 
     console.log("✅ Connected to MongoDB");
   } catch (err) {
     // If connection fails, log the error and exit the app
     console.error("❌ MongoDB connection error:", err);
-    process.exit(1); 
+    process.exit(1);
   }
 }
 
 // Export the function so it can be used in server.js
-module.exports = connectDB;
+module.exports = {connectDB, mongoose};
--- api/routes/user/user_routes.js
+++ api/routes/user/user_routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const User = require("../../schemas/user_schemas"); 
+const User = require("../../schemas/user_schemas");
 
 const router = express.Router();
 
