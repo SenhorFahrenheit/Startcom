77bb94df2fcfc85497082c98bbc42f19ba78bea4
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-09-04 11:40:24 -0300

    Style: Font adjustment(Open Sans import)

---
+++ frontend/src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import './Button.css';
+
+const Button = ({ label, onClick, type = "button", disabled = false, variant}) => {
+  return (
+    <button className={`button ${variant}`} onClick={onClick} type={type} disabled={disabled}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
