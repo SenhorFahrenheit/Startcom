1d8b33b8864701062eb7fd8144f207ffe52615cc
Pedro <pedroh.de.jesus12@gmail.com>
2025-09-13 00:04:53 -0300

    feat(auth): add Google and Apple authentication routes

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-# from routes.login.auth_routes import router as auth_router
+from api.routes.auth.google_routes import router as google_router
+from api.routes.auth.apple_routes import router as apple_router
 from api.routes.user.user_routes import router as user_router
 from api.config.config_cors import setup_cors
 
@@ -9,6 +10,10 @@ app = FastAPI()
 setup_cors(app)
 
 # HTTP routes
-# app.include_router(auth_router)
+app.include_router(google_router, prefix="/auth")
+app.include_router(apple_router, prefix="/auth")
 app.include_router(user_router, prefix="/User")
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("api.main:app", host="127.0.0.1", port=8000, reload=True)
