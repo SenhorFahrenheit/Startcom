3d9db390e0c65db09fc11940f17ca3da28540c7e
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-09-17 23:38:34 -0300

    style: Sidebar 99% (still need logo and responsiveness)

--- frontend/src/layouts/Sidebar/Sidebar.jsx
+++ frontend/src/layouts/Sidebar/Sidebar.jsx
@@ -14,35 +14,37 @@ import { LuLogOut } from 'react-icons/lu';
 const Sidebar = () => {
   return (
     <aside>
-        <div className="logo-sidebar">
-            <h2>StartCom</h2>
+        <div>
+            <div className="logo-sidebar">
+                <h2>StartCom</h2>
+            </div>
+
+            <nav className="nav-sidebar">
+                <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
+                    <BiHomeAlt size={22} /> Início
+                </NavLink>
+
+                <NavLink to="/dashboard" className={({ isActive }) => isActive ? "active" : ""}>
+                    <FaChartColumn size={20} /> Dashboard
+                </NavLink>
+
+                <NavLink to="/vendas" className={({ isActive }) => isActive ? "active" : ""}>
+                    <FiShoppingCart size={20} /> Vendas
+                </NavLink>
+
+                <NavLink to="/clientes" className={({ isActive }) => isActive ? "active" : ""}>
+                    <GoPeople size={20} /> Clientes
+                </NavLink>
+
+                <NavLink to="/estoque" className={({ isActive }) => isActive ? "active" : ""}>
+                    <BsBoxSeam size={20} /> Estoque
+                </NavLink>
+
+                <NavLink to="/relatorios" className={({ isActive }) => isActive ? "active" : ""}>
+                    <FaRegClipboard size={20} /> Relatórios
+                </NavLink>
+            </nav>
         </div>
-
-        <nav className="nav-sidebar">
-            <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>
-                <BiHomeAlt size={22} /> Início
-            </NavLink>
-
-            <NavLink to="/dashboard" className={({ isActive }) => isActive ? "active" : ""}>
-                <FaChartColumn size={20} /> Dashboard
-            </NavLink>
-
-            <NavLink to="/vendas" className={({ isActive }) => isActive ? "active" : ""}>
-                <FiShoppingCart size={20} /> Vendas
-            </NavLink>
-
-            <NavLink to="/clientes" className={({ isActive }) => isActive ? "active" : ""}>
-                <GoPeople size={20} /> Clientes
-            </NavLink>
-
-            <NavLink to="/estoque" className={({ isActive }) => isActive ? "active" : ""}>
-                <BsBoxSeam size={20} /> Estoque
-            </NavLink>
-
-            <NavLink to="/relatorios" className={({ isActive }) => isActive ? "active" : ""}>
-                <FaRegClipboard size={20} /> Relatórios
-            </NavLink>
-        </nav>
 
         <div className="account-block">
             <div className="account-view">
@@ -52,7 +54,7 @@ const Sidebar = () => {
                     <p className="email-sidebar">usuario@gmail.com</p>
                 </div>
             </div>
-            <button><LuLogOut/> Sair</button>
+            <button className="logout-button"><LuLogOut/> Sair</button>
         </div>
     </aside>
   )
