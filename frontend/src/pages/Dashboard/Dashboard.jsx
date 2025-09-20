a2f778dd1deb6ac49abbf0e9feb259ee83e43a0a
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-09-20 13:36:56 -0300

    Feature: FilterDateButton component created

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,19 +1,26 @@
 import formattedDate from "../../utils/formattedDate"
 
 import "./Dashboard.css"
-import "./commonStyle.css"
+import "../commonStyle.css"
 
 import Sidebar from "../../layouts/Sidebar/Sidebar"
+import FilterDateButton from "../../components/filterDateButton/filterDateButton"
 
 const Dashboard = () => {
   return (
-    <section className="body-dashboard">
+    <section className="body-section">
         <Sidebar/>
 
-        <div className="content-dashboard">
-          <div>
-            <h1 className="title-page-section">Dashboard</h1>
-            <p className="description-page-section">Visão geral do seu negócio - {formattedDate()}</p>
+        <div className="content-page-section">
+          <div className="align-heading">
+            <div>
+              <h1 className="title-page-section">Dashboard</h1>
+              <p className="description-page-section">Visão geral do seu negócio - {formattedDate()}</p>
+            </div>
+            <FilterDateButton options={["Hoje", "7 dias", "30 dias", "1 ano", "Período completo"]}
+              defaultValue="30 dias"
+              onSelect={(val) => console.log("Período escolhido:", val)}
+              />
           </div>
         </div>
     </section>
