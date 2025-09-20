d0cdb182bc8e677a27b8fbed287108f734012824
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-09-20 12:36:56 -0300

    Feature: New util function to get current date and a common css file to all pages in navigation

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,22 @@
+import formattedDate from "../../utils/formattedDate"
+
+import "./Dashboard.css"
+import "./commonStyle.css"
+
 import Sidebar from "../../layouts/Sidebar/Sidebar"
 
 const Dashboard = () => {
   return (
-    <div>
+    <section className="body-dashboard">
         <Sidebar/>
-    </div>
+
+        <div className="content-dashboard">
+          <div>
+            <h1 className="title-page-section">Dashboard</h1>
+            <p className="description-page-section">Visão geral do seu negócio - {formattedDate()}</p>
+          </div>
+        </div>
+    </section>
   )
 }
 
