551a56fabd88f40ad9864abdf002f9ff7064ccb6
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-09-21 12:33:34 -0300

    Created Settings Page

---
+++ frontend/src/pages/Settings/Settings.jsx
@@ -0,0 +1,25 @@
+import "./Settings.css"
+import "../commonStyle.css"
+
+import Sidebar from "../../layouts/Sidebar/Sidebar";
+import HeaderMobile from "../../layouts/HeaderMobile/HeaderMobile";
+
+import { useState } from "react";
+
+const Settings = () => {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+      
+    const toggleSidebar = () => {
+        setSidebarOpen(prev => !prev);
+    }
+
+  return (
+    <section className="body-section">
+        <HeaderMobile onToggleSidebar={toggleSidebar} />
+        <Sidebar isOpen={sidebarOpen} onClose={toggleSidebar} />
+    </section>
+  )
+}
+
+export default Settings
