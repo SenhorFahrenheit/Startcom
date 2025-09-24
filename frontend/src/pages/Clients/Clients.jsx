e1786ce3f50b50e5b169f7e177a1e4a96a9e53db
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-09-24 09:28:17 -0300

    feature: ClientModal component

--- frontend/src/pages/Clients/Clients.jsx
+++ frontend/src/pages/Clients/Clients.jsx
@@ -3,11 +3,17 @@ import "../commonStyle.css"
 
 import Sidebar from "../../layouts/Sidebar/Sidebar"
 import HeaderMobile from "../../layouts/HeaderMobile/HeaderMobile";
+import Button from "../../components/Button/Button";
+
+import NewClientModal from "../../components/Modals/ClientModal";
 
 import { useState } from "react";
+import { useAuthModals } from "../../hooks/useAuthModals"
+
+import { LuPlus } from "react-icons/lu";
 
 const Clients = () => {
-
+  const { activeModal, openClient, closeModal } = useAuthModals();
   const [sidebarOpen, setSidebarOpen] = useState(false);
   
     const toggleSidebar = () => {
@@ -18,6 +24,29 @@ const Clients = () => {
     <section className="body-section">
         <HeaderMobile onToggleSidebar={toggleSidebar} />
         <Sidebar isOpen={sidebarOpen} onClose={toggleSidebar} />
+
+        <div className="content-page-section">
+          <div className="align-heading">
+            <div>
+              <h1 className="title-page-section">Clientes</h1>
+              <p className="description-page-section">Gerencie sua base de clientes e relacionamentos</p>
+            </div>
+            <div className="button-shadown">
+              <Button 
+                className="hover-dashboard" 
+                onClick={openClient} 
+                height={40} 
+                width={140} 
+                label={<><LuPlus size={16}/>Nova Cliente</>} 
+              />
+            </div>
+          </div>
+        </div>
+
+      <NewClientModal
+        isOpen={activeModal === "client"}
+        onClose={closeModal}
+      />
     </section>
   )
 }
