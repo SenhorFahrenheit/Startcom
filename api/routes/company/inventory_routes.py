b45ef17ad7d6965e2e1c1d8b6d77aefe4bed4a3e
Yuri Ferreira <yurif9027@gmail.com>
2025-11-01 00:33:52 -0300

    Feat: 'inventory/names' route now it's 'intentory/full' and returns all products and it's info

--- api/routes/company/inventory_routes.py
+++ api/routes/company/inventory_routes.py
@@ -1,21 +1,50 @@
 from fastapi import APIRouter, Depends, status
 from ...infra.database import get_database_client
-from ...schemas.inventory_schemas import InventoryNamesRequest
+from ...schemas.inventory_schemas import InventoryFullRequest
 from ...services.inventory_services import InventoryService
 
 router = APIRouter(prefix="/inventory", tags=["Inventory"])
 
 
-@router.post("/names", status_code=status.HTTP_200_OK)
-async def get_inventory_names_route(
-    body: InventoryNamesRequest,
+@router.post("/full", status_code=status.HTTP_200_OK)
+async def get_inventory_full_route(
+    body: InventoryFullRequest,
     db_client=Depends(get_database_client)
 ):
     """
-    Retrieves all product names from a company's inventory.
+    Returns **all products** from a company's inventory.
 
-    This route is typically used by the frontend to populate dropdowns,
-    search filters, or selection lists when creating sales.
+    Each product includes:
+    - `_id`
+    - `name`
+    - `description`
+    - `price`
+    - `quantity`
+
+    `createdAt` is intentionally excluded.
+
+    Example request:
+    ```json
+    {
+      "companyId": "653b2f9d3e2b123456789012"
+    }
+    ```
+
+    Example response:
+    ```json
+    {
+      "status": "success",
+      "products": [
+        {
+          "_id": "69019f25b407b09e0d09d000",
+          "name": "Notebook Gamer",
+          "description": "Notebook Gamer description",
+          "price": 4500,
+          "quantity": 45
+        }
+      ]
+    }
+    ```
     """
     service = InventoryService(db_client)
-    return await service.get_inventory_names(body.companyId)
+    return await service.get_inventory_full(body.companyId)
