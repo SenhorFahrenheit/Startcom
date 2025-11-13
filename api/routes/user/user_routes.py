95474da354a08d578e4778a6f6e14b654acc3e6e
Yuri Ferreira <yurif9027@gmail.com>
2025-11-13 07:48:43 -0300

    Fix: fixes argon2 error by implementing just bcrypt (projects pattern). Adjust 'User/register' route return and improves documentation

--- api/routes/user/user_routes.py
+++ api/routes/user/user_routes.py
@@ -15,35 +15,51 @@ router = APIRouter(tags=["User"])
 
 @router.post(
     "/register",
-    response_model=UserCreatedResponse,
+    # response_model=UserCreatedResponse,
     status_code=status.HTTP_201_CREATED,
-    summary="Register a new user",
-    description=(
-        "Registers a new user in the system. "
-        "It receives user data (name, email, password, etc.), "
-        "creates the user in the database, and returns the created user's basic info."
-    ),
-    responses={
-        201: {"description": "User successfully created"},
-        400: {"description": "Invalid input data"},
-        409: {"description": "Email or CPF/CPNJ already registered"},
-        500: {"description": "Internal server error"}
-    }
 )
 async def create_user_route(user: UserCreate):
     """
-    Creates a new user based on the provided UserCreate schema.
-    Returns the created user information (without sensitive data).
+    Register a new user in the system.
+
+    ## Request Body
+    ```json
+    {
+      "name": "John Doe",
+      "email": "john@example.com",
+      "password": "strongpassword123",
+      "cpfCnpj": "12345678900"
+    }
+    ```
+
+    ## Successful Response (201)
+    ```json
+    {
+      "id": "653b2f9d3e2b123456789012",
+      "name": "John Doe",
+      "email": "john@example.com",
+      "createdAt": "2025-01-10T14:22:00Z"
+    }
+    ```
+
+    ## Possible Errors
+    - **400 Bad Request** — Invalid data format or missing fields.
+    - **409 Conflict** — Email or CPF/CNPJ already registered.
+    - **500 Internal Server Error** — Unexpected error while creating the user.
+
+    ## Notes
+    - Password is securely hashed before being stored.
+    - Returned data **never** includes sensitive fields (e.g., password).
     """
     try:
         created_user = await user_service.create_user(user)
-        return created_user
+        # return {"status": "success", "created user:": created_user}
+        return {"status": "success", "message":  "User created"}
     except HTTPException as e:
         raise e
     except ValueError as e:
-        # Example: invalid email format or missing field
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
-        # Generic fallback for unexpected server errors
         raise HTTPException(status_code=500, detail=str(e))
+
 
