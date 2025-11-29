5ac7d7262c344d5f8edf7ac1e62fed083a330d9b
Kayle <142769379+KayleDev@users.noreply.github.com>
2025-11-29 01:26:38 -0300

    Docs: Layout comments

--- frontend/src/layouts/Header/Header.jsx
+++ frontend/src/layouts/Header/Header.jsx
@@ -1,24 +1,39 @@
-import { NavLink } from "react-router-dom"
-import Logo from "../../assets/StartComLogo.svg"
+import { NavLink } from "react-router-dom";
+import Logo from "../../assets/StartComLogo.svg";
 
-import Button from "../../components/Button/Button"
-import "./Header.css"
- 
+import Button from "../../components/Button/Button";
+import "./Header.css";
+
+// Header component with logo, navigation links, and dashboard button
 const Header = () => {
   return (
     <header>
-        <div className="logo">
-          <img src={Logo} alt="Logotipo da empresa StartCom"/>
-        </div>
-        <nav className="navigation">
-            <a href="#resources">Recursos</a>
-            <a href="#reviews">Depoimentos</a>
-            <a href="mailto:startcomltda@gmail.com">Contato</a>
-        </nav>
- 
-        <Button styles="mobile-button-none" height={"auto"} width={"170px"} label={<NavLink className="link-inside-button" to="/painel">Acessar Dashboard</NavLink>} type='button' />
+      {/* Company logo */}
+      <div className="logo">
+        <img src={Logo} alt="Logotipo da empresa StartCom" />
+      </div>
+
+      {/* Navigation links */}
+      <nav className="navigation">
+        <a href="#resources">Recursos</a>
+        <a href="#reviews">Depoimentos</a>
+        <a href="mailto:startcomltda@gmail.com">Contato</a>
+      </nav>
+
+      {/* Dashboard access button */}
+      <Button
+        styles="mobile-button-none"
+        height={"auto"}
+        width={"170px"}
+        label={
+          <NavLink className="link-inside-button" to="/painel">
+            Acessar Dashboard
+          </NavLink>
+        }
+        type="button"
+      />
     </header>
-  )
-}
- 
-export default Header
+  );
+};
+
+export default Header;
--- frontend/src/layouts/HeaderMobile/HeaderMobile.jsx
+++ frontend/src/layouts/HeaderMobile/HeaderMobile.jsx
@@ -1,9 +1,11 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import "./HeaderMobile.css";
 
+// Mobile header with a sidebar toggle button
 const HeaderMobile = ({ onToggleSidebar }) => {
   return (
     <header className="header">
+      {/* Hamburger menu button to toggle sidebar */}
       <button className="menu-button" onClick={onToggleSidebar}>
         <GiHamburgerMenu size={22} />
       </button>
